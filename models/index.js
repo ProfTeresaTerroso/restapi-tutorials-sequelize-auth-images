3bd2ee6d477dca34f4afce9aeefc1a78844eb114
ProfTeresaTerroso <teresaterroso@esmad.ipp.pt>
2023-05-30 16:35:01 +0100

    Create user with Sequelize

--- models/index.js
+++ models/index.js
@@ -1,55 +1,55 @@
 const dbConfig = require('../config/index.js');
-// const { Sequelize, DataTypes } = require('sequelize');
+const { Sequelize, DataTypes } = require('sequelize');
 
-// const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-//     host: dbConfig.HOST,
-//     dialect: dbConfig.dialect,
-//     pool: {
-//         max: dbConfig.pool.max,
-//         min: dbConfig.pool.min,
-//         acquire: dbConfig.pool.acquire,
-//         idle: dbConfig.pool.idle
-//     }
-// });
+const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+    host: dbConfig.HOST,
+    dialect: dbConfig.dialect,
+    pool: {
+        max: dbConfig.pool.max,
+        min: dbConfig.pool.min,
+        acquire: dbConfig.pool.acquire,
+        idle: dbConfig.pool.idle
+    }
+});
 
-// (async () => {
-//     try {
-//         console.log(dbConfig)
-//         await sequelize.authenticate;
-//         console.log('Connection has been established successfully.');
-//     } catch (err) {
-//         console.error('Unable to connect to the database:', err);
-//     }
-// })();
+(async () => {
+    try {
+        console.log(dbConfig)
+        await sequelize.authenticate;
+        console.log('Connection has been established successfully.');
+    } catch (err) {
+        console.error('Unable to connect to the database:', err);
+    }
+})();
 
 // const db = {};
 // db.sequelize = sequelize;
 
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
-const db = {};
-db.mongoose = mongoose;
-db.url = dbConfig.MONGO_URL;
+// const db = {};
+// db.mongoose = mongoose;
+// db.url = dbConfig.MONGO_URL;
 
-(async () => {
-    try {
-        console.log(db.url )
-        await db.mongoose.connect(db.url,
-                {
-                    useNewUrlParser: true,
-                    useUnifiedTopology: true
-                }
-            );
-        console.log("Connected to the database!");
-    } catch (error) {
-        console.log("Cannot connect to the database!", error);
-        process.exit();
-    }
-})();
+// (async () => {
+//     try {
+//         console.log(db.url )
+//         await db.mongoose.connect(db.url,
+//                 {
+//                     useNewUrlParser: true,
+//                     useUnifiedTopology: true
+//                 }
+//             );
+//         console.log("Connected to the database!");
+//     } catch (error) {
+//         console.log("Cannot connect to the database!", error);
+//         process.exit();
+//     }
+// })();
 
 // // add User model into DB
-// db.user = require("./user.model.js")(sequelize, DataTypes);
-db.user = require("./user.model.js")(mongoose);
+db.user = require("./user.model.js")(sequelize, DataTypes);
+// db.user = require("./user.model.js")(mongoose);
 
 // // optionally: SYNC
 // (async () => {
--- models/user.model.js
+++ models/user.model.js
@@ -1,55 +1,55 @@
-// module.exports = (sequelize, DataTypes) => {
-//     const User = sequelize.define("user", {
-//         username: {
-//             type: DataTypes.STRING,
-//             unique: true,
-//             allowNull: false,
-//             validate: { notNull: { msg: "Username cannot be empty or null!" } }
-//         },
-//         //new
-//         profile_image: {
-//             type: DataTypes.STRING,
-//         },
-//         cloudinary_id: {
-//             type: DataTypes.STRING,
-//         },
-//         //end_NEW
-//         email: {
-//             type: DataTypes.STRING
-//         },
-//         password: {
-//             type: DataTypes.STRING,
-//             trim: true, // remove spaces on both ends of the string
-//             allowNull: false,
-//             validate: { notNull: { msg: "Password cannot be empty or null!" } }
-//         },
-//         role: {
-//             type: DataTypes.ENUM('admin', 'regular'), // access ENUM values: console.log(User.getAttributes().role.values);
-//             defaultValue: 'regular',
-//             validate: {isIn: {
-//                 args: [['admin', 'regular']],
-//                 msg: "Allowed roles: admin or regular"
-//               }}
-//         }
-//     }, {
-//         timestamps: false
-//     });
-//     return User;
-// };
-
-module.exports = (mongoose) => {
-    const schema = mongoose.Schema(
-            {
-                username:  String,
-                email: String,
-                password: String,
-                role: { type:String, default: "regular"},
-                cloudinary_id: String,
-                profile_image: String
-            },
-            { timestamps: false }
-        );
-    // creates a new model Tutorial using the defined schema above
-    const User = mongoose.model("user", schema);
+module.exports = (sequelize, DataTypes) => {
+    const User = sequelize.define("user", {
+        username: {
+            type: DataTypes.STRING,
+            unique: true,
+            allowNull: false,
+            validate: { notNull: { msg: "Username cannot be empty or null!" } }
+        },
+        //new
+        profile_image: {
+            type: DataTypes.STRING,
+        },
+        cloudinary_id: {
+            type: DataTypes.STRING,
+        },
+        //end_NEW
+        email: {
+            type: DataTypes.STRING
+        },
+        password: {
+            type: DataTypes.STRING,
+            trim: true, // remove spaces on both ends of the string
+            allowNull: false,
+            validate: { notNull: { msg: "Password cannot be empty or null!" } }
+        },
+        role: {
+            type: DataTypes.ENUM('admin', 'regular'), // access ENUM values: console.log(User.getAttributes().role.values);
+            defaultValue: 'regular',
+            validate: {isIn: {
+                args: [['admin', 'regular']],
+                msg: "Allowed roles: admin or regular"
+              }}
+        }
+    }, {
+        timestamps: false
+    });
     return User;
 };
+
+// module.exports = (mongoose) => {
+//     const schema = mongoose.Schema(
+//             {
+//                 username:  String,
+//                 email: String,
+//                 password: String,
+//                 role: { type:String, default: "regular"},
+//                 cloudinary_id: String,
+//                 profile_image: String
+//             },
+//             { timestamps: false }
+//         );
+//     // creates a new model Tutorial using the defined schema above
+//     const User = mongoose.model("user", schema);
+//     return User;
+// };
